97a0046ff304c035cd333bb4b1da36e32dee222c
Gagan-Sky <gagankumar8294@gmail.com>
2024-07-24 16:40:11 +0530

    added code for the input field text displaying using class Component

---
+++ src/Components/InputWithClass.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+export default class Input extends React.Component{
+
+    constructor(){
+        super();
+        this.state = {
+            name: "",
+            lastName: "",
+        }
+    }
+
+    handleName = (e) => {
+        this.setState({
+            name: e.target.value
+        })
+    }
+
+    handleLastName = (e) => {
+        this.setState({
+            lastName: e.target.value
+        })
+    }
+
+    render(){
+        return(
+            <>
+            <div className="section">
+                <Row label="Name">
+                        <input value={this.state.name}
+                        onChange={this.handleName} className="input"/>
+                </Row >
+                <Row label="Last Name">
+                        <input value={this.state.lastName}
+                        onChange={this.handleLastName} className="input"/>
+                </Row >
+            </div>
+    
+            <h2>Hello, { this.state.name + " " + this.state.lastName }</h2>
+            
+            </>
+            )
+
+    }
+   
+    }
+
+
+function Row(props){
+    const{label} = props;
+    return(
+        <>
+        <lable>{label}<br/></lable>
+        {props.children}
+        <hr />
+        </>
+    )
+}
